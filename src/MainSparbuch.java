b08640b322870f51f291b23867215454a151bd6b
Hanna Schultze <HannaSchultzeMV@gmail.com>
2025-06-19 14:21:58 +0200

    OOP für Herr Zickermann Kontonummer angepasst

--- src/MainSparbuch.java
+++ src/MainSparbuch.java
@@ -1,6 +1,6 @@
 public class MainSparbuch {
     public static void main(String[] args) {
-        Sparbuch sb = new Sparbuch(1104711, 1000, 3);
+        Sparbuch sb = new Sparbuch(1104712, 1000, 3);
         sb.zahleEin(60000);
         System.out.printf("Kapital: %.2f € \n", sb.getKapital());
 
